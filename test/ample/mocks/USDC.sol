9af662f3bc16029a3131f702d2a0ed8f5d52e212
Daniel Zarifpour <z@rifpour.xyz>
2025-11-13 15:07:22 -0500

    test(ample): add mocks

---
+++ test/ample/mocks/USDC.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDC is ERC20 {
+    constructor() ERC20("USDC", "USDC") {}
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
